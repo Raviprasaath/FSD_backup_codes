1d643b64fe15370a18f2a2851534a41ae68875f2
Raviprasaath <raviprasaath1@gmail.com>
2024-01-07 08:57:38 +0530

    01-07-202

--- MERN/MovieDeck/Frontend/src/slice/slice.js
+++ MERN/MovieDeck/Frontend/src/slice/slice.js
@@ -37,8 +37,8 @@ const initialState = {
 
 const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 
-// const SERVER_BASE_URL = "http://localhost:4502/"
-const SERVER_BASE_URL = "https://filmfleet-backend-server.onrender.com/"
+const SERVER_BASE_URL = "http://localhost:4502/"
+// const SERVER_BASE_URL = "https://filmfleet-backend-server.onrender.com/"
 
 const API_KEY = '494170c64724d022e9296a5fa98644eb';
 const TMDB_API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTQxNzBjNjQ3MjRkMDIyZTkyOTZhNWZhOTg2NDRlYiIsInN1YiI6IjY0OTAyNGE5MjYzNDYyMDBhZTFjZGI1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7il3x7f91baELU8ceqe8OYauvsHEJ-lC34vS3Gslqoc'
