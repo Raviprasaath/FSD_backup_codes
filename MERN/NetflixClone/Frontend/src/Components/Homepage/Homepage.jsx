ab454334d88c7458a66bfaa3b56a6bdf389765c1
Raviprasaath <raviprasaath1@gmail.com>
2024-01-12 21:55:23 +0530

    01-12-2024

--- MERN/NetflixClone/Frontend/src/Components/Homepage/Homepage.jsx
+++ MERN/NetflixClone/Frontend/src/Components/Homepage/Homepage.jsx
@@ -4,7 +4,6 @@ import { getActionMovie, getAdventureMovie, getAnimationMovie, getComedyMovie, g
 import Carousel from '../Carousel/Carousel';
 import { Suspense } from 'react';
 import Loader from "../LazyLoader/LazyLoader"
-import Shimmer from '../Shimmer/Shimmer';
 import HomeHeaderCarousel from '../HomeHeaderCarousel/HomeHeaderCarousel';
 import { Link } from 'react-router-dom';
 
@@ -64,126 +63,126 @@ const Homepage = () => {
                 <Link to='upcoming/page-1' state={{type: "upcoming"}}>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Upcoming Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={upcomingMovieList}/>                
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='now-showing/page-1' state={{type: "now-showing"}}>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Now Showing</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={nowPlayingMovieList}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='popular/page-1' state={{type: "popular"}}>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Popular Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={popularMovieList}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='top-rated/page-1' state={{type: "top-rated"}}>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Top Rated Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={topRatedMovieList}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='action-movies/page-1' state={{type: "top-rated"}}>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Action Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={actionMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='adventure-movies/page-1' state={{type: "top-rated"}}>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Adventure Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={adventureMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='animation-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Animation Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={animationMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='comedy-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Comedy Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={comedyMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='crime-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Crime Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={crimeMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='documentary-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Documentary Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={DocumentaryMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='drama-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Drama Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={DramaMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='family-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Family Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={FamilyMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='fantasy-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Fantasy Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={FantasyMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='history-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>History Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={HistoryMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='music-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Music Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={MusicMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='mystery-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Mystery Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={MysteryMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='romance-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Romance Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={RomanceMovie}/>
                 </Suspense>
                 <div className={`my-4 border ${screenMode==='dark'?'border-gray-600':'border-gray-200'}  border-[1px]`}></div>
                 <Link to='thriller-movies/page-1'>
                     <h2 className='cursor-pointer font-bold uppercase px-1 py-2 hover:underline'>Thriller Movies</h2>
                 </Link>
-                <Suspense fallback={<Shimmer />}>
+                <Suspense fallback={<Loader />}>
                     <LazyCarousel props={ThrillerMovie}/>
                 </Suspense>
             </div>
