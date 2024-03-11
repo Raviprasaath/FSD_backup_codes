09895cdf6e028be4570be77e765a522bdcd3e986
Raviprasaath <raviprasaath1@gmail.com>
2024-03-11 14:33:28 +0530

    03-11-2024

--- JS_External_Practise/Test/script.js
+++ JS_External_Practise/Test/script.js
@@ -1,1 +1,28 @@
+// Count the occurrence of keys and convert the result into array of objects where each object belongs to one key and it's occurrence (count).
+// Example
+// SHOULD BE CONVERTED TO =
+// [
+    // { language: 'JavaScript', count: 2 },
+    // { language: 'C++', count: 1 },
+    // { language: 'TypeScript', count: 1 }
+// ]
+    
+    
+const languages = [
+    { language: 'JavaScript' }, { language: 'JavaScript' }, { language: 'TypeScript' }, { language: 'C++' }
+] 
 
+
+
+let result = [];
+languages.forEach(({language})=>{
+    console.log(language);
+    let existingItem = result.find((item)=>item.language == language);
+    if(existingItem){
+        existingItem.count++;
+    }else{
+        result.push({language,count:1});
+    }
+})
+
+console.log(result);
