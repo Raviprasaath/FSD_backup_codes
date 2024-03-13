ea512882c64105b8427e664ccd7bc247c0f12ee6
Raviprasaath <raviprasaath1@gmail.com>
2024-03-13 09:16:44 +0530

    03-13-2024

--- JS_External_Practise/CurryingDiffTypes/script.js
+++ JS_External_Practise/CurryingDiffTypes/script.js
@@ -1,32 +1,24 @@
-
 // method 1 by bind
+const multi = function (a, b) {
+    console.log(a*b);
+}
 
-// const multi = function (a, b) {
-//     console.log(a*b);
-// }
-
-// const ans = multi.bind(this, 5)
-// ans(10)  // o/p -> 50
-
-
+const ans = multi.bind(this, 5)
+ans(10)  // o/p -> 50
 
 
 // method 2 in currying
-
 const multiply = function (x) {
     return function (y) {
         console.log(x*y);
     }
 }
 
-const ans = multiply(50);
-ans(4);
-
-
+const ans2 = multiply(50);
+ans2(4);
 
 
 // method 3
-
 const a = function(a) {
     return function (b) {
         return function (c) {
@@ -36,3 +28,4 @@ const a = function(a) {
 }
 
 a(20)(5)(10)
+
